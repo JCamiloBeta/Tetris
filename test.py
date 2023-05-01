5d9c114597c02e484ba963b1117557801498a7d7
Juan Camilo <jbetancurrico@gmail.com>
2023-05-01 01:13:58 -0500

    Se agrego control de movimiento del jugador sobre el tablero

--- test.py
+++ test.py
@@ -14,7 +14,7 @@ tablero = [[0 for j in range(10)] for i in range(20)]
 
 
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
-
+posbot = 50
 while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
@@ -31,13 +31,14 @@ while running:
         for j in range(10):
             posx = 100+j*17
             pygame.draw.rect(screen,"blue",(posx,posy,15,15))
-            
 
-    pygame.draw.circle(screen, "red", player_pos, 40)
+    if(player_pos.x<250 and player_pos.x>100 and player_pos.y<372 and player_pos.y>50):
+        print(player_pos)    
+        pygame.draw.rect(screen,"green",(player_pos.x,player_pos.y,15,15))
+    # pygame.draw.circle(screen, "red", player_pos, 15)
 
     pygame.draw.rect(screen, "black", [98, 48, 172, 342], 2)
-
-
+    
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
         player_pos.y -= 300 * dt
@@ -54,6 +55,6 @@ while running:
     # limits FPS to 60
     # dt is delta time in seconds since last frame, used for framerate-
     # independent physics.
-    dt = clock.tick(60) / 1000
+    dt = clock.tick(60) / 2500
 
 pygame.quit()
